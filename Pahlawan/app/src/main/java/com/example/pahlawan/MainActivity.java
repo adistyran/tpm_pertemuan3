4eb50f05a3214512c366229c4bf1ec6705b02e1e
adistyran <adistyrania19@gmail.com>
2020-02-24 12:30:12 +0700

    tpm pertemuan 3

---
+++ Pahlawan/app/src/main/java/com/example/pahlawan/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.pahlawan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rvHero;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHero = findViewById(R.id.activitymain_rv_hero);
+        rvHero.setHasFixedSize(true);
+
+        showRecyclerList();
+    }
+
+
+    private void showRecyclerList(){
+        rvHero.setLayoutManager(new LinearLayoutManager(this));
+
+        HeroesAdapter heroesAdapter = new HeroesAdapter(this);
+        heroesAdapter.setHeroes(HeroesData.getHeroesList());
+
+        rvHero.setAdapter(heroesAdapter);
+    }
+
+}
